b561fd0881824256ad6412d8b7aaff8b113e806b
Ivan Carrillo <helloivancar@gmail.com>
2022-06-03 14:40:07 -0700

    Add outline of code

--- server/server.js
+++ server/server.js
@@ -4,9 +4,55 @@ const app = express();
 const port = 4000;
 
 const client = new Client({
-    node: 'http://localhost:9200'
+  node: 'http://localhost:9200'
+});
+
+
+// Post request will receive data from search form
+app.post('/search', (req, res) => {
+  // let searchString = req.body.text;
+
+  // Call elasticsearch with the search string here
+
+  /*
+  const results = await client.search({
+    index: 'page-body',
+    query: {
+      match: { 
+        quote: searchString
+      }
+    }
   });
+  */
+
+  let results = 
+  [
+    {
+      title: "UCR Article",
+      text: "This is an example article at UCR"
+    },
+    {
+      title: "UCR Graduation",
+      text: "How to graduate"
+    }
+  ]
+
+  res.json(results);
+})
+
+function indexPages() {
+  // Do indexing stuff here
+  
+  // foreach file
+    // load file
+    // add file to index??
+    // close file
+
+  // refresh index
+  
+}
 
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
 });
+
