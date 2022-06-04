49c4fe29a6901a886370fd1ca54a5b9ca5d24a65
Ivan Carrillo <helloivancar@gmail.com>
2022-06-03 21:19:56 -0700

    more stuff

--- server/server.js
+++ server/server.js
@@ -12,21 +12,9 @@ const client = new Client({
 // Post request will receive data from search form and return results
 app.get('/search', async (req, res) => {
   // let searchString = req.body.text;
-  let searchString = "Spanish" // temporary testing search string
+  let searchString = "test" // temporary testing search string
 
   const results = await searchPages(searchString);
-
-  // let results = 
-  // [
-  //   {
-  //     title: "UCR Article",
-  //     text: "This is an example article at UCR"
-  //   },
-  //   {
-  //     title: "UCR Graduation",
-  //     text: "How to graduate"
-  //   }
-  // ]
 
   res.json(results);
 })
@@ -36,15 +24,15 @@ async function searchPages(searchString) {
     index: 'pages',
     query: {
       match: { 
-        quote: searchString
+        content: searchString
       }
     }
   });
-  console.log(result.hits.hits);
+  console.log(result.hits);
 }
 
 async function indexPages() {
-  fs.readdirSync("../data").forEach(file => {
+  fs.readdirSync("../data").forEach( async (file) =>  {
     let fileContent = fs.readFileSync("../data/" + file, {encoding: "utf8"});
     // console.log(file)
     // console.log(fileContent)
@@ -61,50 +49,8 @@ async function indexPages() {
   
 }
 
+// client.indices.delete("pages")
 indexPages();
-
-async function run () {
-  // Let's start by indexing some data
-  await client.index({
-    index: 'game-of-thrones',
-    document: {
-      character: 'Ned Stark',
-      quote: 'Winter is coming.'
-    }
-  })
-
-  await client.index({
-    index: 'game-of-thrones',
-    document: {
-      character: 'Daenerys Targaryen',
-      quote: 'I am the blood of the dragon.'
-    }
-  })
-
-  await client.index({
-    index: 'game-of-thrones',
-    document: {
-      character: 'Tyrion Lannister',
-      quote: 'A mind needs books like a sword needs a whetstone.'
-    }
-  })
-
-  // here we are forcing an index refresh, otherwise we will not
-  // get any result in the consequent search
-  await client.indices.refresh({ index: 'game-of-thrones' })
-
-  // Let's search!
-  const result= await client.search({
-    index: 'game-of-thrones',
-    query: {
-      match: { quote: 'winter' }
-    }
-  })
-
-  console.log(result.hits.hits)
-}
-
-run().catch(console.log)
 
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
